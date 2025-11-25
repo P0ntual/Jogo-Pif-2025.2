53c2b041452cd6f676348163056e825c3580ed4b
hugo <hugotngomes@outlook.com>
2025-11-25 20:02:46 -0300

    complemento do sistema de ranking

---
+++ includes/ranking.h
@@ -0,0 +1,28 @@
+#ifndef RANKING_H
+#define RANKING_H
+
+#include "raylib.h"
+
+#define MAX_SCORES 10
+#define MAX_NAME_LENGTH 20
+#define RANKING_FILE "ranking.json"
+
+typedef struct {
+    char nome[MAX_NAME_LENGTH];
+    float pontuacao;
+} Score;
+
+typedef struct {
+    Score scores[MAX_SCORES];
+    int count;
+} Ranking;
+
+void InitRanking();
+void UnloadRanking();
+void CarregarRanking(Ranking* ranking);
+void SalvarRanking(Ranking* ranking);
+void AdicionarScore(Ranking* ranking, const char* nome, float pontuacao);
+void DesenharRanking(Ranking* ranking, Font fonte, float x, float y, float tamanhoFonte);
+
+#endif
+
