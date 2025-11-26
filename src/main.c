7e4ae380282f01a5c5e39ba51c21f11bc936fa5d
hugo <hugotngomes@outlook.com>
2025-11-26 15:46:21 -0300

    ajeitando o jeito que o nome do jogo aparece

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@ int main(void)
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "Meu Jogo Vertical");
+    InitWindow(screenWidth, screenHeight, "Fly");
     
     
     InitAudio();
